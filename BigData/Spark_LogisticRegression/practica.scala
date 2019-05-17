8244c8102b4b229b073889d1c6160a37eb0c2d64
Oscar Barraza <ruzziantaco@gmail.com>
2019-05-16 17:43:10 -0700

    Practicas kmeans y regresion lineal

--- BigData/Spark_LogisticRegression/practica.scala
+++ BigData/Spark_LogisticRegression/practica.scala
@@ -1,6 +1,9 @@
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j._
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+
 
 Logger.getLogger("org").setLevel(Level.ERROR)
 
@@ -21,3 +24,22 @@ import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer, VectorIndexe
 import org.apache.spark.ml.linalg.Vectors
 
 val assembler = (new VectorAssembler().setInputCols(Array("Daily Time Spent on Site","Age", "Area Income","Daily Internet Usage","Hour")).setOutputCol("features"))
+val Array(training, test) = datos.randomSplit(Array(0.7, 0.3), seed = 12345)
+
+
+val lr = new LogisticRegression()
+
+val pipeline = new Pipeline().setStages(Array(assembler,lr))
+
+val model = pipeline.fit(training)
+
+val results = model.transform(test)
+
+val predictionAndLabels = results.select($"prediction",$"label").as[(Double, Double)].rdd
+val metrics = new MulticlassMetrics(predictionAndLabels)
+
+// Matriz de confusion
+println("Confusion matrix:")
+println(metrics.confusionMatrix)
+
+metrics.accuracy
